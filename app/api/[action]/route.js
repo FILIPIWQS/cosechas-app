ba40ed86188bd19186955907892419490df4ec44
FILIPIWQS <filipiyvf@hotmail.com>
2026-06-22 18:17:10 -0300

    Foto COCO RALADO 500g

--- app/api/[action]/route.js
+++ app/api/[action]/route.js
@@ -103,7 +103,7 @@ const SEED = [
   { name: 'COPO PARA PROVA', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_609928-MLB73541781451_122023-F-copo-descartavel-50ml-cafe-cafezinho-5000-uni-caixa-full.webp' },
   { name: 'COLHER PEQUENA', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_817496-MLB89138203383_082025-F.webp' },
   { name: 'COCO EM LASCAS 500g', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_810220-MLB74700135866_022024-F.webp' },
-  { name: 'COCO RALADO 500g', image: 'https://res.cloudinary.com/dxv7tpwlo/image/upload/v1/foto_22.jpg' },
+  { name: 'COCO RALADO 500g', image: 'https://casaspedro.vteximg.com.br/arquivos/ids/214206-550-550/coco-ralado-grosso-kg.png?v=639116946775800000' },
   { name: 'CAFÉ SOLUVEL', image: 'https://res.cloudinary.com/dxv7tpwlo/image/upload/v1/foto_23.jpg' },
   { name: 'AMENDOIM PICADO 500g', image: 'https://res.cloudinary.com/dxv7tpwlo/image/upload/v1/foto_24.jpg' },
   { name: 'Açucar Cristal 1KG', image: 'https://res.cloudinary.com/dxv7tpwlo/image/upload/v1/foto_25.jpg' },
